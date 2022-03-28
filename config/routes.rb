0e8fdd6dcfcfc147e9439d4619cfa45efc7e2986
barefoot-yeti <groenie007@gmail.com>
2022-03-28 22:13:30 +0200

    Heroku roll back, working app

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  root 'cars#index'
-  resources :cars
+  resources :posts do
+    resources :comments
 end
-
-#Rails.application.routes.draw do
-#  resources :posts do
-#    resources :comments
-#end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
- #  root "posts#index"
-#end
+  root "posts#index"
+end
