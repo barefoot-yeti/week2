1bac944a9d43801b56b1c53b46299ca0691e0ae6
barefoot-yeti <groenie007@gmail.com>
2022-04-11 22:24:18 +0200

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,10 +66,11 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  #config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Amazon SES SMTP Config
-  # config.action_mailer.default_url_options = { host: 'Amazon SES' }
+  config.action_mailer.default_url_options = { host: 'mcweek2.herokuapp.com', protocol: "https" }
+  
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
